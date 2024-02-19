e723d56672438833579379cde75130b3004add5c
Aparecido Ribeiro <aparecido.silva12y@gmail.com>
2024-02-19 10:55:03 -0300

    feat: adicionei algumas perguntas na feature de date

--- src/features/date.jsx
+++ src/features/date.jsx
@@ -2,19 +2,44 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
     {
-        pergunta: "Qual minha idade?",
-        altenativas: [12, 13, 18, 15],
-        correta: 18
+        pergunta: "A partir de 1889, a cidade passou a ser conhecida como 'Araripe'. Mas como era denominada até então?",
+        altenativas: [
+            'Chapada do Araripe',
+            'Brejo Seco',
+            'Riacho Grande',
+            'Brejinho'
+        ],
+        correta: 'Brejo Seco'
     },
     {
-        pergunta: "Que ano estamos?",
-        altenativas: [2000, 2012, 2022, 2024],
-        correta: 2024
+        pergunta: "Em 1899, a vila de Araripe tem seu território anexado a outro município e se torna distrito de qual cidade?",
+        altenativas: [
+            'Campos Sales',
+            'Salitre',
+            'Alagoinha',
+            'Pajeú'
+        ],
+        correta: 'Campos Sales'
     },
     {
-        pergunta: "Em que ano nasci?",
-        altenativas: [2005, 2012, 2022, 2024],
-        correta: 2005
+        pergunta: "Qual o nome do evento junino que se destaca na cidade pela dança conhecida como quadrilha?",
+        altenativas: [
+            'Festa na Roça',
+            'Quadrilha Junina Raiz',
+            'Forró Junino',
+            'Fequaripe'
+        ],
+        correta: 'Fequaripe'
+    },
+    {
+        pergunta: "No mês de junho acontece um evento religioso chamado a 'festa do padroeiro' que ocorre em devoção a qual santo?",
+        altenativas: [
+            'Frei Damião',
+            'São João',
+            'Santo Antônio',
+            'Santa Rita'
+        ],
+        correta: 'Santo Antônio'
     },
 ]
 
